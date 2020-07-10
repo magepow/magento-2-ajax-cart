2c593f60586e62139f4307e9b310cae82944528e
Alex Dong <support@alothemes.com>
2020-07-10 10:14:21 +0700

    Clean code

---
+++ Block/System/Config/Form/Field/ColorPicker.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @Author: Alex Dong
+ * @Date:   2020-07-09 22:24:37
+ * @Last Modified by:   nguyen
+ * @Last Modified time: 2020-07-10 09:28:42
+ */
+
+namespace Magepow\Ajaxcart\Block\System\Config\Form\Field;
+
+class ColorPicker extends \Magento\Config\Block\System\Config\Form\Field
+{
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+    \Magento\Backend\Block\Template\Context $context,
+    array $data = []
+    ) 
+    {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * add color picker in admin configuration fields
+     * @param  \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string script
+     */
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $html = $element->getElementHtml();
+        $value = $element->getData('value');
+    
+        $html .= '<script type="text/javascript">
+            require(["jquery", "jquery/colorpicker/js/colorpicker"], function ($) {
+                $(document).ready(function (e) {
+                    var $el = $("#' . $element->getHtmlId() . '");
+                    $el.css("backgroundColor", "'. $value .'");
+
+                    // Attach the color picker
+                    $el.ColorPicker({
+                        color: "'. $value .'",
+                        onChange: function (hsb, hex, rgb) {
+                            $el.css("backgroundColor", "#" + hex).val("#" + hex);
+                        }
+                    });
+                });
+            });
+            </script>';
+
+        return $html;
+    }
+}
