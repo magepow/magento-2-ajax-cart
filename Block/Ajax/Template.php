47adfb8293c7f54651eaf6c924d6bf97889500f3
Alex Dong <support@alothemes.com>
2020-08-09 08:10:32 +0700

    Compatible old version

---
+++ Block/Ajax/Template.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * @category: Magepow
+ * @copyright: Copyright (c) 2014 Magepow (http://www.magepow.com/)
+ * @licence: http://www.magepow.com/license-agreement
+ * @author: MichaelHa
+ * @create date: 2019-06-14 17:19:50
+ * @LastEditors: MichaelHa
+ * @LastEditTime: 2019-06-29 12:44:29
+ */
+namespace Magepow\Ajaxcart\Block\Ajax;
+
+class Template extends \Magepow\Ajaxcart\Block\Ajax
+{
+
+}
